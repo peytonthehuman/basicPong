7ec22e632debc4458ef48323b4bb3ebc498c34c8
peytonthehuman <33160477+peytonthehuman@users.noreply.github.com>
2017-11-09 11:39:14 -0600

    Moved font class headers to new seperate file

---
+++ pong_font.h
@@ -0,0 +1,35 @@
+#ifndef PONG_FONT_H
+#define PONG_FONT_H
+
+// character object, which is used to display characters on screen
+class character {
+	private:
+		int w;
+		int h;
+		char id;
+		bool* map;
+	public:
+		character();
+		void init(char inId, bool* inMap, int inW, int inH);
+
+		int getW() const;
+		int getH() const;
+		char getID() const;
+		bool* getMap() const;
+
+		~character();
+};
+
+class font { // font class
+	private:
+		character* charArray;
+		int numChars;
+	public:
+		font();
+		character& getChar(char id);
+		bool getCharAt(char id, int index);
+		bool getCharAt(char id, int x, int y);
+		~font();
+};
+
+#endif
